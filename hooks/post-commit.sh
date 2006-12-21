a130e86cedd0154a1fec423fc8d1f448b58e589e
svn <svn@6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46>
2006-12-21 07:46:09 +0000

    send error mails to cvs-admin@.
    git-svn-id: svn+ssh://ci.ruby-lang.org/scripts/trunk@9 6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46

--- hooks/post-commit.sh
+++ hooks/post-commit.sh
@@ -10,7 +10,7 @@ REV="$2"
    "$REPOS" "$REV" ruby-cvs@ruby-lang.org \
    -I /export/home/svn/scripts/svn-utils/lib \
    --name Ruby \
-   --error-to admin@ruby-lang.org
+   --error-to cvs-admin@ruby-lang.org
 
 #   --from admin@ruby-lang.org
 #   -r http://svn.ruby-lang.org/repos/ruby \
