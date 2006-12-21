bccb1217b5fac4c6e44fb8f42c953dbe99e9e138
svn <svn@6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46>
2006-12-21 06:17:04 +0000

    do not use ~. (/bin/sh doesn't interpret it)
    git-svn-id: svn+ssh://ci.ruby-lang.org/scripts/trunk@3 6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46

--- hooks/post-commit.sh
+++ hooks/post-commit.sh
@@ -3,9 +3,9 @@
 REPOS="$1"
 REV="$2"
 
-~svn/scripts/svn-utils/bin/commit-email.rb \
+/export/home/svn/scripts/svn-utils/bin/commit-email.rb \
    "$REPOS" "$REV" ruby-cvs@ruby-lang.org \
-   -I ~svn/scripts/svn-utils/lib \
+   -I /export/home/svn/scripts/svn-utils/lib \
    -r http://svn.ruby-lang.org/repos/ruby \
    --name Ruby \
    --error-to admin@ruby-lang.org \
@@ -14,4 +14,4 @@ REV="$2"
 #   --rss-path ~/ruby.rdf \
 #   --rss-uri http://svn.ruby-lang.org/rss/ruby.rdf \
 
-~svn/scripts/cia/ciabot_svn.py "$REPOS" "$REV" ruby &
+/export/home/svn/scripts/cia/ciabot_svn.py "$REPOS" "$REV" ruby &
