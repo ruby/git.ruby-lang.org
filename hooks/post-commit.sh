ade6854f2c2dced1a63e273b614557a9a94fb882
svn <svn@6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46>
2006-12-21 06:58:09 +0000

    removed -r.
    git-svn-id: svn+ssh://ci.ruby-lang.org/scripts/trunk@5 6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46

--- hooks/post-commit.sh
+++ hooks/post-commit.sh
@@ -8,11 +8,11 @@ REV="$2"
 /export/home/svn/scripts/svn-utils/bin/commit-email.rb \
    "$REPOS" "$REV" ruby-cvs@ruby-lang.org \
    -I /export/home/svn/scripts/svn-utils/lib \
-   -r http://svn.ruby-lang.org/repos/ruby \
    --name Ruby \
    --error-to admin@ruby-lang.org \
    --from admin@ruby-lang.org
 
+#   -r http://svn.ruby-lang.org/repos/ruby \
 #   --rss-path ~/ruby.rdf \
 #   --rss-uri http://svn.ruby-lang.org/rss/ruby.rdf \
 
