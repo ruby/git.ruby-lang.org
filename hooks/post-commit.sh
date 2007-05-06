1bd5c90ac222120fb0edf4aaf5debbf79c313fb8
eban <eban@6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46>
2007-05-06 16:16:52 +0000

    * post-commit.sh: add dump. disable cia bot.
    git-svn-id: svn+ssh://ci.ruby-lang.org/scripts/trunk@17 6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46

--- hooks/post-commit.sh
+++ hooks/post-commit.sh
@@ -6,15 +6,25 @@ export PATH
 REPOS="$1"
 REV="$2"
 
+svnadmin dump -q -r "$REV" --incremental "$REPOS" | bzip2 -c > /var/svn/dump/ruby/$REV.bz2
+
 /export/home/svn/scripts/svn-utils/bin/commit-email.rb \
    "$REPOS" "$REV" ruby-cvs@ruby-lang.org \
    -I /export/home/svn/scripts/svn-utils/lib \
    --name Ruby \
+   --viewvc-uri http://svn.ruby-lang.org/cgi-bin/viewvc.cgi \
    --error-to cvs-admin@ruby-lang.org
+
+#/export/home/svn/scripts/svn-utils/bin/commit-email-test.rb \
+#   "$REPOS" "$REV" eban@ruby-lang.org \
+#   -I /export/home/svn/scripts/svn-utils/lib \
+#   --name Ruby \
+#   --viewvc-uri http://svn.ruby-lang.org/cgi-bin/viewvc.cgi \
+#   --error-to eban@ruby-lang.org
 
 #   --from admin@ruby-lang.org
 #   -r http://svn.ruby-lang.org/repos/ruby \
 #   --rss-path ~/ruby.rdf \
 #   --rss-uri http://svn.ruby-lang.org/rss/ruby.rdf \
 
-/export/home/svn/scripts/cia/ciabot_svn.py "$REPOS" "$REV" ruby &
+#/export/home/svn/scripts/cia/ciabot_svn.py "$REPOS" "$REV" ruby &
