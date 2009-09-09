ae2a99bb4dcbda95809f70d504f6cd2d922fbbf5
eban <eban@6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46>
2009-09-09 07:41:08 +0000

    * svn-utils/bin/commit-email.rb: lstrip.
    git-svn-id: svn+ssh://ci.ruby-lang.org/scripts/trunk@59 6a1fb5df-bfb0-4272-e4b2-8d85fa3eee46

--- svn-utils/bin/commit-email.rb
+++ svn-utils/bin/commit-email.rb
@@ -84,7 +84,7 @@ def make_body(info, params)
   body << change_info(info, params[:viewvc_uri])
   body << "\n"
   body << "  Log:\n"
-  body << info.log.sub(/\A\n+/).gsub(/^\t*/, "    ").rstrip
+  body << info.log.lstrip.gsub(/^\t*/, "    ").rstrip
   body << "\n\n"
   body << added_dirs(info)
   body << added_files(info)
