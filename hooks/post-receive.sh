021465e367500286f08d28aa03e416e1ff127ce6
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2018-10-20 15:43:30 +0900

    Move github sync script into cgit hook from svn.

--- hooks/post-receive.sh
+++ hooks/post-receive.sh
@@ -6,3 +6,14 @@ set -o pipefail
 # Until migration to git is finished, we should develop `hooks/post-receive-pre.sh` and test it
 # on `git@git.ruby-lang.org:ruby.pre.git`.
 # Make sure this script is executed asynchronously using `&`, since this script is a little slow.
+
+# script parameters
+ruby_git="/var/git/ruby.git"
+hook_log="/tmp/post-receive.log"
+
+{ date; echo '### start ###'; uptime; } >> "$hook_log"
+
+{ date; echo github sync; uptime; } >> "$hook_log"
+git remote update; git push github
+
+{ date; echo '### end ###'; uptime; } >> "$hook_log"
