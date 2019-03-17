60a3033eb338bb786bb5e5c0a1d3b6ac973ad05d
Yusuke Endoh <mame@ruby-lang.org>
2019-03-18 01:09:12 +0900

    notify-slack.rb: Show the branch name

--- notify-slack.rb
+++ notify-slack.rb
@@ -7,13 +7,17 @@ require "json"
 SLACK_WEBHOOK_URL = File.read(File.expand_path("~git/config/slack-webhook")).chomp
 
 ARGV.each_slice(3) do |oldrev, newrev, refname|
-  out, = Open3.capture2("git", "log", "--pretty=format:%H\n%h\n%an\n%at\n%cn\n%ct\n%B", "-z", ARGV[0] + ".." + ARGV[1])
+  out, = Open3.capture2("git", "rev-parse", "--symbolic", "--abbrev-ref", refname)
+  branch = out.strip
+
+  out, = Open3.capture2("git", "log", "--pretty=format:%H\n%h\n%an\n%at\n%cn\n%ct\n%B", "-z", oldrev + ".." + newrev)
+
   attachments = []
   out.split("\0").reverse_each do |s|
     hash, abbr_hash, _author, _authortime, committer, committertime, body = s.split("\n", 7)
     subject, body = body.split("\n", 2)
     attachments << {
-      title: "#{ abbr_hash } (#{ refname }): #{ subject }",
+      title: "#{ abbr_hash } (#{ branch }): #{ subject } (#{ oldrev } #{ newrev } #{ refname })",
       title_link: "https://github.com/ruby/ruby/commit/" + hash,
       text: (body || "").strip,
       footer: committer,
