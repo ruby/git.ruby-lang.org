e97efec8f849bc4323760af3b98486b37cb0a491
Yusuke Endoh <mame@ruby-lang.org>
2019-03-18 01:20:42 +0900

    Deploy notify-slack.rb in production

--- hooks/post-receive.sh
+++ hooks/post-receive.sh
@@ -10,6 +10,7 @@ set -o pipefail
 # script parameters
 ruby_git="/var/git/ruby.git"
 hook_log="/tmp/post-receive.log"
+ruby_commit_hook="$(cd "$(dirname $0)"; cd ..; pwd)"
 
 { date; echo '### start ###'; uptime; } >> "$hook_log"
 
@@ -18,5 +19,8 @@ curl "https://bugs.ruby-lang.org/sys/fetch_changesets?key=`cat ~git/config/redmi
 
 { date; echo github sync; uptime; } >> "$hook_log"
 git remote update; git push github
+
+{ date; echo notify slack; uptime; } >> "$hook_log"
+$ruby_commit_hook/notify-slack.rb $*
 
 { date; echo '### end ###'; uptime; } >> "$hook_log"
