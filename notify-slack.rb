d474ea0d764df162721c08218780d23c8191d6cf
Yusuke Endoh <mame@ruby-lang.org>
2019-03-18 14:15:55 +0900

    notify-slack.rb: notify to multiple channels

--- notify-slack.rb
+++ notify-slack.rb
@@ -4,7 +4,10 @@ require "net/https"
 require "open3"
 require "json"
 
-SLACK_WEBHOOK_URL = File.read(File.expand_path("~git/config/slack-webhook")).chomp
+SLACK_WEBHOOK_URLS = [
+  File.read(File.expand_path("~git/config/slack-webhook-alerts")).chomp,
+  File.read(File.expand_path("~git/config/slack-webhook-commits")).chomp,
+]
 
 def escape(s)
   s.gsub(/[&<>]/, "&" => "&amp;", "<" => "&lt;", ">" => "&gt;")
@@ -20,7 +23,7 @@ ARGV.each_slice(3) do |oldrev, newrev, refname|
   out.split("\0").reverse_each do |s|
     hash, abbr_hash, _author, _authortime, committer, committertime, body = s.split("\n", 7)
     subject, body = body.split("\n", 2)
-    body = body.strip.sub(%r(git-svn-id: svn\+ssh://ci\.ruby-lang\.org/ruby/trunk@\d+ \h+-\h+-\h+-\h+-\h+\z), "").strip
+    body = body.strip.sub(%r(git-svn-id: svn\+ssh://ci\.ruby-lang\.org/ruby/trunk@(\d+) \h+-\h+-\h+-\h+-\h+\z)) { "(r#$1)" }.strip
     attachments << {
       title: "#{ abbr_hash } (#{ branch }): #{ escape(subject) }",
       title_link: "https://github.com/ruby/ruby/commit/" + hash,
@@ -37,12 +40,14 @@ ARGV.each_slice(3) do |oldrev, newrev, refname|
   #  JSON.generate(json),
   #  "Content-Type" => "application/json"
   #)
-  uri = URI.parse(SLACK_WEBHOOK_URL)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.start do
-    req = Net::HTTP::Post.new(uri.path)
-    req.set_form_data(payload: json.to_json)
-    http.request(req)
+  SLACK_WEBHOOK_URLS.each do |url|
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.start do
+      req = Net::HTTP::Post.new(uri.path)
+      req.set_form_data(payload: json.to_json)
+      http.request(req)
+    end
   end
 end
