1377ba7ad7841fb78e48786601cabb326a495485
Takashi Kokubun <takashikkbn@gmail.com>
2019-04-21 12:15:42 +0900

    Revert "Eliminate sha256 interface"
    This reverts commit 4883ea7b8b78e4144f1ea0b6a317285327d0119a.

--- svn-utils/bin/commit-email.rb
+++ svn-utils/bin/commit-email.rb
@@ -6,13 +6,19 @@ require "ostruct"
 SENDMAIL = "/usr/sbin/sendmail"
 
 CommitEmailInfo = Struct.new(
-  :author, :log, :date,
-  :added_files, :deleted_files, :updated_files,
-  :added_dirs, :deleted_dirs, :updated_dirs,
+  :author,
+  :log,
+  :date,
+  :added_files,
+  :deleted_files,
+  :updated_files,
+  :added_dirs,
+  :deleted_dirs,
+  :updated_dirs,
   :diffs,
   :revision,
+  :sha256,
   :entire_sha256,
-  :branches,
 )
 
 class GitInfoBuilder
@@ -237,6 +243,7 @@ def make_header(to, from, info, params)
   headers << x_repository(info)
   headers << x_revision(info)
   headers << x_id(info)
+#  headers << x_sha256(info)
   headers << "Content-Type: text/plain; charset=us-ascii"
   headers << "Content-Transfer-Encoding: 7bit"
   headers << "From: #{from}"
@@ -248,7 +255,7 @@ def make_header(to, from, info, params)
 end
 
 def make_subject(name, info)
-  branches = info.branches
+  branches = info.sha256.map{|x,| x[/(?:branches\/|tags\/)?(.+?)\//, 1]}.uniq
   subject = ""
   subject << "#{info.author}:"
   subject << "r#{info.revision}"
@@ -272,6 +279,12 @@ end
 
 def x_revision(info)
   "X-SVN-Revision: #{info.revision}"
+end
+
+def x_sha256(info)
+  info.sha256.collect do |name, inf|
+    "X-SVN-SHA256-Info: #{name}, #{inf[:revision]}, #{inf[:sha256]}"
+  end.join("\n")
 end
 
 def make_mail(to, from, info, params)
