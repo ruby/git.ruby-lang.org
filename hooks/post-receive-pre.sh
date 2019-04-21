1ba7f243882b816efb7e5c3d89cafea5e5a07e1d
Takashi Kokubun <takashikkbn@gmail.com>
2019-04-21 10:55:02 +0900

    Set -x for debugging in pre

--- hooks/post-receive-pre.sh
+++ hooks/post-receive-pre.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 set -o pipefail
 # This script is executed by `git@git.ruby-lang.org:ruby.pre.git/hooks/post-receive`.
 # The ruby.pre repository is just a sandbox, and any commit isn't pushed to it automatically.
