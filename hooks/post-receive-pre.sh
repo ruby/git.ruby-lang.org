3bc3d9fc6b27f752339eb3e3aace5cf5978a7baf
Takashi Kokubun <takashikkbn@gmail.com>
2019-04-21 17:50:27 +0900

    Revert "Debug commit-email in pre again"
    This reverts commit b419e4dd2097e49a8a462a56d69f66f30a4d4d06.

--- hooks/post-receive-pre.sh
+++ hooks/post-receive-pre.sh
@@ -15,18 +15,18 @@ ruby_commit_hook="$(cd "$(dirname $0)"; cd ..; pwd)"
 { date; echo '### start ###'; uptime; } >> "$hook_log"
 export RUBY_GIT_HOOK=1 # used by auto-style.rb
 
-{ date; echo commit-email.rb; uptime; } >> "$hook_log"
-"${ruby_commit_hook}/bin/commit-email.rb" \
-   "$ruby_git" ruby-cvs@ruby-lang.org $* \
-   -I "${ruby_commit_hook}/lib" \
-   --name Ruby \
-   --viewer-uri "https://git.ruby-lang.org/ruby.git/commit/?id=" \
-   -r https://svn.ruby-lang.org/repos/ruby \
-   --rss-path /tmp/ruby.rdf \
-   --rss-uri https://svn.ruby-lang.org/rss/ruby.rdf \
-   --error-to cvs-admin@ruby-lang.org \
-   --vcs git \
-   > /tmp/post-receive-pre-commit-email.log 2>&1
+# { date; echo commit-email.rb; uptime; } >> "$hook_log"
+# "${ruby_commit_hook}/bin/commit-email.rb" \
+#    "$ruby_git" ruby-cvs@ruby-lang.org $* \
+#    -I "${ruby_commit_hook}/lib" \
+#    --name Ruby \
+#    --viewer-uri "https://git.ruby-lang.org/ruby.git/commit/?id=" \
+#    -r https://svn.ruby-lang.org/repos/ruby \
+#    --rss-path /tmp/ruby.rdf \
+#    --rss-uri https://svn.ruby-lang.org/rss/ruby.rdf \
+#    --error-to cvs-admin@ruby-lang.org \
+#    --vcs git \
+#    > /tmp/post-receive-pre-commit-email.log 2>&1
 
 { date; echo auto-style; uptime; } >> "$hook_log"
 "${ruby_commit_hook}/bin/auto-style.rb" "$ruby_git"
