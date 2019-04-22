e9e11469ebafd9b6e90f2e9cf523ae4374f2eac4
Yusuke Endoh <mame@ruby-lang.org>
2019-04-22 23:14:35 +0900

    check-email.rb: check only the last commit in pushed commits
    To allow merge commit

--- bin/check-email.rb
+++ bin/check-email.rb
@@ -28,31 +28,29 @@ end
 
 emails = SVN_TO_EMAILS[svn_account_name]
 
-ARGV.each_slice(3) do |oldrev, newrev, refname|
-  out, = Open3.capture2("git", "log", "--pretty=format:%H\n%ce", "-z", oldrev + ".." + newrev)
+ARGV.each_slice(3) do |_oldrev, newrev, _refname|
+  out, = Open3.capture2("git", "show", "-s", "--pretty=format:%H\n%ce", newrev)
 
-  out.split("\0").reverse_each do |s|
-    hash, git_committer_email = s.split("\n")
-    if emails
-      if emails == git_committer_email || emails.include?(git_committer_email)
-        # OK
-      else
-        puts "Your ssh key is not associated to the git committer email."
-        puts "Please see https://github.com/ruby/ruby-commit-hook/blob/master/bin/check-email.rb"
-        puts "and send PR, or contact on ruby-core@ruby-lang.org."
-        exit 1 # NG
-      end
+  hash, git_committer_email = out.split("\n")
+  if emails
+    if emails == git_committer_email || emails.include?(git_committer_email)
+      # OK
     else
-      if Time.now > Time.new(2020, 1, 1)
-        puts "Your ssh key is unknown."
-        puts "Please see https://github.com/ruby/ruby-commit-hook/blob/master/bin/check-email.rb"
-        puts "and send PR, or contact on ruby-core@ruby-lang.org."
-        exit 1 # NG
-      else
-        # Until the last of 2019, we record the association of SVN_ACCOUNT_NAME and GIT_COMMITTER_EMAIL.
-        open(LOG_FILE, "a") do |f|
-          f.puts "#{ hash } #{ svn_account_name } #{ git_committer_email }"
-        end
+      puts "Your ssh key is not associated to the git committer email."
+      puts "Please see https://github.com/ruby/ruby-commit-hook/blob/master/bin/check-email.rb"
+      puts "and send PR, or contact on ruby-core@ruby-lang.org."
+      exit 1 # NG
+    end
+  else
+    if Time.now > Time.new(2020, 1, 1)
+      puts "Your ssh key is unknown."
+      puts "Please see https://github.com/ruby/ruby-commit-hook/blob/master/bin/check-email.rb"
+      puts "and send PR, or contact on ruby-core@ruby-lang.org."
+      exit 1 # NG
+    else
+      # Until the last of 2019, we record the association of SVN_ACCOUNT_NAME and GIT_COMMITTER_EMAIL.
+      open(LOG_FILE, "a") do |f|
+        f.puts "#{ hash } #{ svn_account_name } #{ git_committer_email }"
       end
     end
   end
