3c0f0a4ad6060d1ded38fcdb499d58cda979b586
Takashi Kokubun <takashikkbn@gmail.com>
2019-04-28 00:52:28 +0900

    Use the same logging format between pre and non-pre

--- hooks/post-receive-pre.sh
+++ hooks/post-receive-pre.sh
@@ -8,13 +8,12 @@ ruby_git="/var/git/ruby.pre.git"
 hook_log="/tmp/post-receive-pre.log"
 ruby_commit_hook="$(cd "$(dirname $0)"; cd ..; pwd)"
 
-{ date; echo '### start ###'; uptime; } >> "$hook_log"
+{ date; echo; echo '### start ###'; uptime; } >> "$hook_log"
 
-# TODO: Enable the following code when in production
-# { date; echo github sync 1; uptime; } >> "$hook_log"
+# { date; echo '==> github sync 1'; uptime; } >> "$hook_log"
 # git remote update; git push github
 
-# { date; echo commit-email.rb; uptime; } >> "$hook_log"
+# { date; echo '==> commit-email.rb'; uptime; } >> "$hook_log"
 # "${ruby_commit_hook}/bin/commit-email.rb" \
 #    "$ruby_git" ruby-cvs@ruby-lang.org $* \
 #    -I "${ruby_commit_hook}/lib" \
@@ -27,23 +26,21 @@ ruby_commit_hook="$(cd "$(dirname $0)"; cd ..; pwd)"
 #    --vcs git \
 #    > /tmp/post-receive-pre-commit-email.log 2>&1
 
-{ date; echo auto-style; uptime; } >> "$hook_log"
+{ date; echo '==> auto-style'; uptime; } >> "$hook_log"
 SVN_ACCOUNT_NAME=git "${ruby_commit_hook}/bin/auto-style.rb" "$ruby_git" \
    >> "$hook_log" 2>&1
 
-{ date; echo update-version.h.rb; uptime; } >> "$hook_log"
+{ date; echo '==> update-version.h.rb'; uptime; } >> "$hook_log"
 SVN_ACCOUNT_NAME=git "${ruby_commit_hook}/bin/update-version.h.rb" git "$ruby_git" $* \
    >> "$hook_log" 2>&1
 
-{ date; echo redmine fetch changesets; uptime; } >> "$hook_log"
+{ date; echo '==> redmine fetch changesets'; uptime; } >> "$hook_log"
 curl "https://bugs.ruby-lang.org/sys/fetch_changesets?key=`cat ~git/config/redmine.key`" &
 
-# TODO: Enable the following code when in production
-# { date; echo github sync 2; uptime; } >> "$hook_log"
+# { date; echo '==> github sync 2'; uptime; } >> "$hook_log"
 # git remote update; git push github
 
-# TODO: Enable the following code when in production
-# { date; echo notify slack; uptime; } >> "$hook_log"
+# { date; echo '==> notify slack'; uptime; } >> "$hook_log"
 # $ruby_commit_hook/bin/notify-slack.rb $*
 
 { date; echo '### end ###'; uptime; } >> "$hook_log"
