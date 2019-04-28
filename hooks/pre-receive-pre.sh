ccf0391c6e9062ae13a13d8ba01ba318b59b2a8a
Takashi Kokubun <takashikkbn@gmail.com>
2019-04-28 14:49:07 +0900

    Show hook log by caller side

--- hooks/pre-receive-pre.sh
+++ hooks/pre-receive-pre.sh
@@ -2,10 +2,10 @@
 set -o pipefail
 # This script is executed by `git@git.ruby-lang.org:ruby.pre.git/hooks/pre-receive`.
 # The ruby.pre repository is just a sandbox, and any commit isn't pushed to it automatically.
+# Its outputs are logged to `/tmp/pre-receive-pre.log`.
 
 # script parameters
 ruby_git="/var/git/ruby.git"
-hook_log="/tmp/pre-receive-pre.log"
 ruby_commit_hook="$(cd "$(dirname $0)"; cd ..; pwd)"
 
 $ruby_commit_hook/bin/prohibit-merge-commits.rb $* || exit 1
--- hooks/pre-receive.sh
+++ hooks/pre-receive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eu
 set -o pipefail
 # This script is executed by `git@git.ruby-lang.org:ruby.git/hooks/pre-receive`.
+# Its outputs are logged to `/tmp/pre-receive.log`.
 
 # script parameters
 ruby_git="/var/git/ruby.git"
-hook_log="/tmp/pre-receive.log"
 ruby_commit_hook="$(cd "$(dirname $0)"; cd ..; pwd)"
 
 $ruby_commit_hook/bin/prohibit-merge-commits.rb $* || exit 1
