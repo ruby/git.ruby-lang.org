323266c1534ac49d39057fe972fb7b365d1b11bd
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-03 10:25:33 -0500

    Perform rejection only when revision is correctly parsed

--- bin/prohibit-merge-commits.rb
+++ bin/prohibit-merge-commits.rb
@@ -3,11 +3,13 @@
 require "open3"
 
 ARGV.each_slice(3) do |oldrev, newrev, refname|
-  out, = Open3.capture2("git", "rev-list", "--parents", oldrev + ".." + newrev)
-  out.lines.each do |s|
-    if s.split.size >= 3
-      puts "A merge commit is prohibited."
-      exit 1
+  out, _err, status = Open3.capture3("git", "rev-list", "--parents", "#{oldrev}..#{newrev}")
+  if status.success?
+    out.lines.each do |s|
+      if s.split.size >= 3
+        puts "A merge commit is prohibited."
+        exit 1
+      end
     end
   end
 end
