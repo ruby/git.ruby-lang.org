089b033c18e9f106122f971267239a732ba2cdcb
Takashi Kokubun <takashikkbn@gmail.com>
2019-06-13 21:32:36 +0900

    Override nagachika-san's svn email gravatar

--- bin/notify-slack.rb
+++ bin/notify-slack.rb
@@ -10,6 +10,7 @@ SLACK_WEBHOOK_URLS = [
   File.read(File.expand_path("~git/config/slack-webhook-commits")).chomp,
 ]
 GRAVATAR_OVERRIDES = {
+  "nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e" => "https://avatars0.githubusercontent.com/u/21976",
   "svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e" => "https://avatars1.githubusercontent.com/u/29403229",
   "usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e" => "https://avatars2.githubusercontent.com/u/17790",
   "usa@ruby-lang.org" => "https://avatars2.githubusercontent.com/u/17790",
