4f68330d382b1e17d0409b88fb9b93a8c834a0b2
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-12 13:40:41 +0900

    Lock may not be needed
    unless we do `git gc`.

--- bin/update-ruby-commit-hook.sh
+++ bin/update-ruby-commit-hook.sh
@@ -2,6 +2,4 @@
 # This is executed as `sudo -u git /home/git/ruby-commit-hook/bin/update-ruby-commit-hook.sh`
 # when GitHub ruby-commit-hook's push webhook is delivered to `cgi-bin/webhook.cgi`.
 
-# TODO: maybe better to take flock because concurrent `git pull` can be dangerous.
-
 git -C /home/git/ruby-commit-hook pull origin master
