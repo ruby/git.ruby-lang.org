a5744fc4fbf402d6a6cddfd953776fc21d069032
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-12 20:24:57 +0900

    Add basic logging

--- hooks/update.sh
+++ hooks/update.sh
@@ -7,4 +7,14 @@ refname="$1"
 oldrev="$2"
 newrev="$3"
 
+function log() {
+  echo -e "[$$: $(date "+%Y-%m-%d %H:%M:%S %Z")] $1"
+}
+
+log "### start ###"
+log "args: $*"
+
+log "==> git push github"
 git push github "$newrev:$refname"
+
+log "### end ###\n"
