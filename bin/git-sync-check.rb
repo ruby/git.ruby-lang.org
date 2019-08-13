529a298e5a5b22a5948cdbd7db131b597f201677
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-13 20:01:04 +0900

    Check USER just in case

--- bin/git-sync-check.rb
+++ bin/git-sync-check.rb
@@ -2,4 +2,4 @@
 # This is executed by `/lib/systemd/system/git-sync-check.service` which is
 # triggered every 10 minutes by `/lib/systemd/system/git-sync-check.timer`.
 
-puts "hello world"
+puts "hello world #{ENV['USER']}"
