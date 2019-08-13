ef5a12c6ec8fdeb331b6087b1e44144de6d92189
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-13 19:59:33 +0900

    Apparently systemd does not allow symlink

--- bin/git-sync-check.rb
+++ bin/git-sync-check.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
+# This is executed by `/lib/systemd/system/git-sync-check.service` which is
+# triggered every 10 minutes by `/lib/systemd/system/git-sync-check.timer`.
 
 puts "hello world"
