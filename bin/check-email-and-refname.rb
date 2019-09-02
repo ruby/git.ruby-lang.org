a7e20d0e05eaf05207ed6f48803c627ccaada6aa
Takashi Kokubun <takashikkbn@gmail.com>
2019-09-02 15:54:58 +0900

    Carve out config/email.yml from check-email

--- bin/check-email-and-refname.rb
+++ bin/check-email-and-refname.rb
@@ -1,42 +1,15 @@
 #!/usr/bin/env ruby
 
 require "open3"
+require "yaml"
 
-# This is the correspondence table between SVN_ACCOUNT_NAME and GIT_COMMITTER_EMAIL.
-# SVN_ACCOUNT_NAME is set in ~git/.ssh/authorized_keys.
-#
-# You can add a new email address but DO NOT DELETE already-registered email address.
-# This association will be used for committer identification of the ruby repository.
+ADMIN_USERS = [
+  "git",
+  "hsbt",
+  "naruse",
+]
 
-SVN_TO_EMAILS = {
-  "aycabta" => ["aycabta@gmail.com"],
-  "duerst" => ["duerst@it.aoyama.ac.jp"],
-  "eregon" => ["eregontp@gmail.com", "eregon@ruby-lang.org"],
-  "git" => :admin,
-  "hsbt" => :admin,
-  "jeremy" => ["code@jeremyevans.net"],
-  "k0kubun" => ["takashikkbn@gmail.com"],
-  "kazu" => ["zn@mbf.nifty.com"],
-  "ko1" => ["ko1@atdot.net"],
-  "kou" => ["kou@cozmixng.org", "kou@clear-code.com"],
-  "ktsj" => ["kazuki@callcc.net"],
-  "mame" => ["mame@ruby-lang.org"],
-  "nagachika" => ["nagachika@ruby-lang.org"],
-  "naruse" => :admin,
-  "nobu" => ["nobu@ruby-lang.org"],
-  "samuel" => ["samuel.williams@oriontransfer.co.nz"],
-  "seki" => ["m_seki@mva.biglobe.ne.jp"],
-  "sorah" => ["her@sorah.jp", "sorah@cookpad.com"],
-  "shugo" => ["shugo@ruby-lang.org"],
-  "shyouhei" => ["shyouhei@ruby-lang.org"],
-  "stomar" => ["sto.mar@web.de"],
-  "suke" => ["masaki.suketa@nifty.ne.jp"],
-  "tadd" => ["tad.a.digger@gmail.com", "tadd@ruby-lang.org"],
-  "tenderlove" => ["aaron.patterson@gmail.com", "tenderlove@ruby-lang.org"],
-  "usa" => ["usa@ruby-lang.org"],
-  "yugui" => ["yugui@yugui.jp"],
-}
-
+SVN_TO_EMAILS = YAML.safe_load(File.read(File.expand_path('../config/email.yml', __dir__)))
 LOG_FILE = "/home/git/email.log"
 
 svn_account_name = ENV["SVN_ACCOUNT_NAME"]
@@ -47,9 +20,9 @@ if svn_account_name.nil?
   exit 1
 end
 
-emails = SVN_TO_EMAILS[svn_account_name]
+exit 0 if ADMIN_USERS.include?(svn_account_name)
 
-exit 0 if emails == :admin
+emails = SVN_TO_EMAILS[svn_account_name]
 
 pushable_refnames = [
   'refs/heads/master',
@@ -75,14 +48,14 @@ ARGV.each_slice(3) do |oldrev, newrev, refname|
         # OK
       else
         STDERR.puts "The git committer email (#{git_committer_email}) does not seem to be #{svn_account_name}'s email (#{emails.join(', ')})."
-        STDERR.puts "Please see https://github.com/ruby/ruby-commit-hook/blob/master/bin/check-email-and-refname.rb"
+        STDERR.puts "Please see https://github.com/ruby/ruby-commit-hook/blob/master/config/email.yml"
         STDERR.puts "and send PR, or contact on ruby-core@ruby-lang.org."
         exit 1 # NG
       end
     else
       if Time.now > Time.new(2020, 1, 1)
         STDERR.puts "Your ssh key is unknown."
-        STDERR.puts "Please see https://github.com/ruby/ruby-commit-hook/blob/master/bin/check-email-and-refname.rb"
+        STDERR.puts "Please see https://github.com/ruby/ruby-commit-hook/blob/master/config/email.yml"
         STDERR.puts "and send PR, or contact on ruby-core@ruby-lang.org."
         exit 1 # NG
       else
