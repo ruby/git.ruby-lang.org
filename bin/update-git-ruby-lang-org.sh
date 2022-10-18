77d670c2905fc65a4160223a5a6cfec54fff328d
Takashi Kokubun <takashikkbn@gmail.com>
2022-10-17 21:03:25 -0700

    Update its own reference

--- bin/update-git-ruby-lang-org.sh
+++ bin/update-git-ruby-lang-org.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eu
-# This is executed as `sudo -u git /home/git/ruby-commit-hook/bin/update-ruby-commit-hook.sh`
-# when GitHub ruby-commit-hook's push webhook is delivered to `cgi-bin/webhook.cgi`.
+# This is executed as `sudo -u git /home/git/git.ruby-lang.org/bin/update-git-ruby-lang-org.sh`
+# when GitHub ruby/git.ruby-lang.org's push webhook is delivered to `cgi-bin/webhook.cgi`.
 #
 # This supports only updating master branch for now.
 
-git -C /home/git/ruby-commit-hook pull origin master
+git -C /home/git/git.ruby-lang.org pull origin master
