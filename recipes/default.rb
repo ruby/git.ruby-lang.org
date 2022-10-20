0508b0945daef1e876b1bd5f4a1034790d7724c9
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-10-20 19:15:30 +0900

    restore git user

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,16 @@ package "postfix"
 package "gpg"
 package "rsync"
 
+user "git" do
+  shell "/usr/bin/git-shell"
+end
+
+directory "/home/git" do
+  owner "git"
+  group "git"
+  mode "0755"
+end
+
 include_recipe 'apache2'
 include_recipe 'git-sync-check'
 
