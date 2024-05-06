b02f2ba510b639eb8f27e5c13b4d7589899cc784
Takashi Kokubun <takashikkbn@gmail.com>
2024-05-06 00:47:08 -0700

    Add a workaround for git/bullseye-backports

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,9 @@ include_recipe 'bullseye-backports'
 
 package 'cgit'
 package 'certbot'
-package 'git/bullseye-backports'
+package 'git/bullseye-backports' do
+  only_if '[[ $(git -v) != "git version 2.39.2" ]]'
+end
 package 'ruby'
 package 'postfix'
 package 'gpg'
