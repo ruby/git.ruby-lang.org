c51dd519b8067830ead3341f4f04bbb98ba0acef
Takashi Kokubun <takashikkbn@gmail.com>
2025-10-07 21:23:52 -0700

    Migrate the log storage for update-ruby.sh to GitHub Actions
    from /tmp/update-ruby.log in git.ruby-lang.org

--- bin/update-ruby.sh
+++ bin/update-ruby.sh
@@ -12,10 +12,9 @@ export LANG=en_US.UTF-8
 ruby_repo="/var/git/ruby.git"
 ruby_branch="$(basename "$1")"
 ruby_workdir="/data/git.ruby-lang.org/update-ruby"
-log_path="/tmp/update-ruby.log"
 
 function log() {
-  echo -e "[$$: $(date "+%Y-%m-%d %H:%M:%S %Z")] $1" >> "$log_path"
+  echo -e "[$$: $(date "+%Y-%m-%d %H:%M:%S %Z")] $1"
 }
 
 # Initialize working directory only if missing
@@ -28,7 +27,7 @@ log "### start ###"
 
 # Sync: GitHub -> ruby_workdir -> cgit
 # By doing this way, we can make sure all git hooks are performed on sync-ed commits.
-git -C "$ruby_workdir" fetch github "$ruby_branch" >> "$log_path" 2>&1
-SVN_ACCOUNT_NAME=git git -C "$ruby_workdir" push origin "github/${ruby_branch}:${ruby_branch}" >> "$log_path" 2>&1
+git -C "$ruby_workdir" fetch github "$ruby_branch"
+SVN_ACCOUNT_NAME=git git -C "$ruby_workdir" push origin "github/${ruby_branch}:${ruby_branch}"
 
 log "### end ###\n"
