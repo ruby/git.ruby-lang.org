e2bb85c6f3f04c3cb2c1cff8f5e574ddf41cbae0
Takashi Kokubun <takashikkbn@gmail.com>
2025-10-07 21:50:11 -0700

    Use set -x for logging update-ruby.sh instead

--- bin/update-ruby.sh
+++ bin/update-ruby.sh
@@ -13,21 +13,15 @@ ruby_repo="/var/git/ruby.git"
 ruby_branch="$(basename "$1")"
 ruby_workdir="/data/git.ruby-lang.org/update-ruby"
 
-function log() {
-  echo -e "[$$: $(date "+%Y-%m-%d %H:%M:%S %Z")] $1"
-}
-
 # Initialize working directory only if missing
 if [ ! -d "$ruby_workdir" ]; then
   git clone "file://${ruby_repo}" "$ruby_workdir"
   git -C "$ruby_workdir" remote add github https://github.com/ruby/ruby
 fi
 
-log "### start ###"
+set -eux
 
 # Sync: GitHub -> ruby_workdir -> cgit
 # By doing this way, we can make sure all git hooks are performed on sync-ed commits.
 git -C "$ruby_workdir" fetch github "$ruby_branch"
 SVN_ACCOUNT_NAME=git git -C "$ruby_workdir" push origin "github/${ruby_branch}:${ruby_branch}"
-
-log "### end ###\n"
