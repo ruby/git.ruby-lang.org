77967dc2d73fb34409482633717b23f90c6d82c4
Takashi Kokubun <takashikkbn@gmail.com>
2026-05-19 16:30:10 -0700

    Allow merge commits in stable branches

--- bin/prohibit-merge-commits.rb
+++ bin/prohibit-merge-commits.rb
@@ -6,8 +6,9 @@ ARGV.each_slice(3) do |oldrev, newrev, refname|
   out, _err, status = Open3.capture3("git", "rev-list", "--parents", "#{oldrev}..#{newrev}")
   if status.success?
     out.lines.each do |s|
-      if s.split.size >= 3
-        STDERR.puts "A merge commit is prohibited."
+      # Allow merge commits in stable branches to preserve history for stable releases
+      if s.split.size >= 3 && !File.basename(refname).match?(/\Aruby_\d+_\d+\z/)
+        STDERR.puts "A merge commit is prohibited for #{refname}."
         exit 1
       end
     end
